109cf4c5d3514eb403ae9d78c9833090b289a960
Abhay Bhagat <abhay.bhagat12@gmail.com>
2024-06-10 19:25:21 -0400

    commit linked list

---
+++ src/LinkedList/DoublyLinkedList.java
@@ -0,0 +1,147 @@
+class ListNode{
+
+    private int value;
+    private ListNode next;
+    private ListNode previous;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public ListNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(ListNode previous) {
+        this.previous = previous;
+    }
+}
+public class DoublyLinkedList {
+    ListNode head;
+    public void display(){
+
+        ListNode node = head;
+        while (node != null){
+            System.out.print(node.getValue() + " <=> ");
+            node = node.getNext();
+        }
+        System.out.println(" END");
+
+    }
+
+    public int get(int index) {
+
+        ListNode node = head;
+        int counter = 0;
+
+        while (node != null){
+            if(counter == index){
+                return node.getValue();
+            }
+            node = node.getNext();
+            counter ++;
+        }
+
+        return -1;
+    }
+
+    public void addAtHead(int val) {
+
+        if(head == null){
+            head = new ListNode(val);
+            return;
+        }
+        ListNode node = new ListNode(val);
+        node.setNext(head);
+        head.setPrevious(node);
+        head = node;
+
+    }
+
+    public void addAtTail(int val) {
+
+        if(head == null){
+            head = new ListNode(val);
+            return;
+        }
+        ListNode node = new ListNode(val);
+        ListNode current = head;
+
+        while (current.getNext() != null){
+            current = current.getNext();
+        }
+        current.setNext(node);
+        node.setPrevious(current);
+
+    }
+
+    public void addAtIndex(int index, int val) {
+
+        ListNode current = head;
+        int position = 0;
+
+        if(index == 0){
+            addAtHead(val);
+            return;
+        }
+        while(current != null){
+            if(position == index - 1){
+
+                ListNode newNode = new ListNode(val);
+                if(current.getNext() != null){
+                    newNode.setNext(current.getNext());
+                    current.getNext().setPrevious(newNode);
+                }
+                newNode.setPrevious(current);
+                current.setNext(newNode);
+                return;
+
+            }
+            position++;
+            current = current.getNext();
+
+        }
+        System.out.println("Index out of bounds");
+
+    }
+
+    public void deleteAtIndex(int index) {
+        ListNode current = head;
+        int position = 0;
+        if(index == 0){
+            head = head.getNext();
+            return;
+        }
+        while(current != null){
+            if(position == index){
+                if(current.getNext() != null){
+                    current.getNext().setPrevious(current.getPrevious());
+                }
+                current.getPrevious().setNext(current.getNext());
+                return;
+            }
+            position++;
+            current = current.getNext();
+        }
+        System.out.println("Index out of bounds");
+
+
+    }
+
+}
---
+++ src/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,143 @@
+public class SinglyLinkedList {
+
+    ListNode head;
+
+    private class ListNode{
+
+        private int value;
+        private ListNode next;
+
+        public ListNode(int value) {
+            this.value = value;
+        }
+        public ListNode(int value, ListNode next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+
+    public void display(){
+
+        ListNode node = head;
+        while (node != null){
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.println(" END");
+
+    }
+
+    public int get(int index) {
+        ListNode node = head;
+        int position = 0;
+        int val = -1;
+        while(node != null){
+            if(position == index){
+                val = node.value;
+                return val;
+            }
+            node = node.next;
+            position ++;
+        }
+        return val;
+    }
+
+    public void addAtHead(int val) {
+        ListNode node = new ListNode(val);
+        node.next = head;
+        head = node;
+    }
+    public void addAtTail(int val) {
+        if(head == null){
+            head = new ListNode(val);
+            return;
+        }
+        ListNode newNode = new ListNode(val);
+        ListNode node = head;
+
+        while(node.next != null){
+            node = node.next;
+        }
+        node.next = newNode;
+    }
+
+    public void addAtIndex(int index, int val) {
+        ListNode node = head;
+        int position = 0;
+        if(index == 0){
+            addAtHead(val);
+            return;
+        }
+        while(node != null){
+            if(position == index - 1){
+                ListNode newNode = new ListNode(val);
+                newNode.next = node.next;
+                node.next = newNode;
+                return;
+            }
+            position++;
+            node = node.next;
+        }
+        System.out.println("Index out of bounds");
+    }
+
+    public void deleteAtIndex(int index) {
+        ListNode node = head;
+        int position = 0;
+        if(index == 0){
+            head = head.next;
+            return;
+        }
+        while(node != null){
+
+            if(position == index - 1) {
+                if (node.next != null){
+                    node.next = node.next.next;
+                }else{
+                    node.next = null;
+                }
+                return;
+            }
+            position++;
+            node = node.next;
+        }
+        System.out.println("Index out of bounds");
+    }
+
+}
+
+/*
+
+Sample main function
+
+public static void main(String[] args) {
+
+    SinglyLinkedList sll = new SinglyLinkedList();
+    sll.addAtHead(1);
+    sll.addAtTail(3);
+    sll.addAtIndex(1,2);
+    System.out.println("value at index 1 is : " + sll.get(1));
+    sll.deleteAtIndex(0);
+    System.out.println("value at index 0 is : " + sll.get(0));
+
+//        sll.addAtHead(2);
+//        sll.addAtHead(1);
+//        sll.addAtIndex(3,0);
+//        sll.deleteAtIndex(2);
+//        sll.addAtHead(6);
+//        sll.addAtTail(4);
+//        System.out.println("value at index 4 is : " + sll.get(4));
+//        sll.addAtHead(4);
+//        sll.addAtIndex(5,0);
+//        sll.addAtHead(6);
+
+    //  6  1  2    0  4
+
+    sll.display();
+
+//        System.out.println("value at index 3 is : " + sll.get(3));
+
+
+
+}*/
