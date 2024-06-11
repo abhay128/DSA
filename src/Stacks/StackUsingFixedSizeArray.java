708c31e1155fbec36f54c8bcc62cdb1a5f8fd412
Abhay Bhagat <abhay.bhagat12@gmail.com>
2024-06-10 21:16:48 -0400

    implemented stack using fixed size array

--- src/Stacks/StackUsingFixedSizeArray.java
+++ src/Stacks/StackUsingFixedSizeArray.java
@@ -1,10 +1,77 @@
+package Stacks;
 
-import Stacks.StackUsingFixedSizeArray;
+public class StackUsingFixedSizeArray {
+    private int capacity;           // store array capacity
+    private int top;                // store array index of the top element
+    private int[] integerStack;     // declare integer array for the stack
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+    public StackUsingFixedSizeArray(int capacity) {     // Initialize
+        this.capacity = capacity;
+        this.top = -1;
+        this.integerStack = new int[capacity];
+    }
+
+    public void display(){
+        for(int i = 0 ; i<= top ; i++){
+            System.out.print(integerStack[i] + " -> ");
+        }
+        System.out.println(" Top ");
+    }
+    public void push(int data){
+
+        if(top >= capacity - 1){            // check for stack overflow
+            System.out.println(" ***** Stack Overflow *****");
+        }else{
+            top++;
+            integerStack[top] = data;
+        }
+    }
+
+    public void pop(){
+        if(top == -1){
+            System.out.println(" ***** Stack Underflow *****");
+        }else{
+            top--;
+        }
+    }
+
+    public int size(){
+        return top + 1;
+    }
+
+    public boolean isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull(){
+        if(top == capacity - 1){
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteStack(){
+        top = -1;
+        integerStack = null;
+    }
+
+    public int top(){
+        if(top != -1){
+            return integerStack[top];
+        }
+        System.out.println("Stack is empty");
+        return -1;
+    }
+
+}
+/*
+
+Sample main function for various test cases
+
+ public static void main(String[] args) {
         int stackCapacity = 5;
         StackUsingFixedSizeArray stack = new StackUsingFixedSizeArray(stackCapacity);
         stack.display();
@@ -56,4 +123,5 @@ public class Main {
         stack.pop();
 
     }
-}
+
+ */
