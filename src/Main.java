6d6bd6d02149c56ba9739b7d85f97ab7f35176b3
Abhay Bhagat <abhay.bhagat12@gmail.com>
2024-12-22 17:06:19 -0500

    Binary tree using array - insert node method

--- src/Main.java
+++ src/Main.java
@@ -7,51 +7,18 @@ import Trees.TreeNode;
 public class Main {
     public static void main(String[] args) {
 
-        BinaryTreeUsingLinkedList binaryTreeUsingLinkedList = new BinaryTreeUsingLinkedList();
-        TreeNode treeNode1 = new TreeNode();
-        treeNode1.value = "1";
-        TreeNode treeNode2 = new TreeNode();
-        treeNode2.value = "2";
-        TreeNode treeNode3 = new TreeNode();
-        treeNode3.value = "3";
-        TreeNode treeNode4 = new TreeNode();
-        treeNode4.value = "4";
-        TreeNode treeNode5 = new TreeNode();
-        treeNode5.value = "5";
-        TreeNode treeNode6 = new TreeNode();
-        treeNode6.value = "6";
-        TreeNode treeNode7 = new TreeNode();
-        treeNode7.value = "7";
-        TreeNode treeNode8 = new TreeNode();
-        treeNode8.value = "8";
-        TreeNode treeNode9 = new TreeNode();
-        treeNode9.value = "9";
-        TreeNode treeNode10 = new TreeNode();
-        treeNode10.value = "10";
-
-        TreeNode treeNode11 = new TreeNode();
-        treeNode11.value = "11";
-
-        treeNode1.left = treeNode2;
-        treeNode1.right = treeNode3;
-
-        treeNode2.left = treeNode4;
-        treeNode2.right = treeNode5;
-
-        treeNode3.left = treeNode6;
-        treeNode3.right = treeNode7;
-
-        treeNode4.left = treeNode8;
-
-        treeNode6.left = treeNode9;
-        treeNode6.right = treeNode10;
-
-        binaryTreeUsingLinkedList.root = treeNode1;
 
         System.out.println("\n");
 
         int size = 5;
         BinaryTreeUsingArray binaryTreeUsingArray = new BinaryTreeUsingArray(size);
+
+        binaryTreeUsingArray.insertNode("1");
+        binaryTreeUsingArray.insertNode("2");
+        binaryTreeUsingArray.insertNode("3");
+        binaryTreeUsingArray.insertNode("4");
+        binaryTreeUsingArray.insertNode("5");
+        binaryTreeUsingArray.insertNode("6");
 
 
 
--- src/Trees/BinaryTreeUsingArray.java
+++ src/Trees/BinaryTreeUsingArray.java
@@ -9,11 +9,28 @@ right child = 2 * parent_index + 1
 public class BinaryTreeUsingArray {
 
     String[] binaryTreeArray;
-    int lastUsedINdex;
+    int lastUsedIndex;
 
-    public BinaryTreeUsingArray(int size){
+    public BinaryTreeUsingArray(int size){                           //Time Complexity : O(1) , Space Complexity: O(N)
         binaryTreeArray = new String[size + 1];
-        this.lastUsedINdex = 0;
+        this.lastUsedIndex = 0;
         System.out.println(" Binary tree of size " + size + " has been created");
+    }
+
+    public boolean isFull(){                                        //Time Complexity : O(1) , Space Complexity: O(1)
+        if(lastUsedIndex == binaryTreeArray.length - 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void insertNode(String value){                           //Time Complexity : O(1) , Space Complexity: O(1)
+        if(!isFull()){
+            lastUsedIndex += 1;
+            binaryTreeArray[lastUsedIndex] = value;
+            System.out.println("The value of " + value + " has been added");
+        }else{
+            System.out.println("The Binary Tree is full");
+        }
     }
 }
